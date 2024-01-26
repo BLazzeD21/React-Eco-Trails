084f076a10f25ac5868d7fed7cf8b8e9db9d6827
Alexandr Sekerin <blazzed21@gmail.com>
2024-01-26 22:14:37 +0300

    feat: add data to point page

--- src/pages/ShowPoint.jsx
+++ src/pages/ShowPoint.jsx
@@ -26,9 +26,26 @@ const ShowPoint = () => {
           <div className="point__img">
             <img src={image} alt={name} />
           </div>
-          <p className="point__descr">
-            {description}
-          </p>
+          <div className="point__descr">
+            <p>{description}</p>
+          </div>
+          {services ? ( <div className="point__services">
+            <p>
+              <b>Услуги: </b>{services}
+            </p>
+          </div>) : ''}
+          {infrastructure ? ( <div className="point__infrastructure">
+            <p>
+              <b>Инфраструктура: </b>{infrastructure}
+            </p>
+          </div>) : ''}
+          <div className="point__address">
+            <p>
+              <b>Адрес: </b>{address}
+              <br />
+              <b>Координаты: </b>{`[${coordinates[0]}, ${coordinates[1]}]`}
+            </p>
+          </div>
         </div>
       </Container>
     </div>
